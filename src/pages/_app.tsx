a5e2b9fff2d8b4808b89d26bdb790a4461c1f9c6
0xTranqui <thinkandchill11@gmail.com>
2023-05-26 12:45:02 -0400

    more seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -69,8 +69,8 @@ function App({ Component, pageProps }: AppProps) {
                   <meta name="twitter:card" content="summary_large_image" />
                   <meta name="twitter:image" content={defaultOGImage} />
                   <meta property="og:image" content={defaultOGImage} />
-                  <meta property="og:image:width" content="2058" />
-                  <meta property="og:image:height" content="1080" />                
+                  <meta property="og:image:width" content="1048" />
+                  <meta property="og:image:height" content="540" />                
                 </NextHead>
                 <Header />
                 <Footer />
